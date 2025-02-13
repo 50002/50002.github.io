5aecb3749199642ada70550b4c5311cf43f42be8
Matias Ylinen <50002@edu.raseko.fi>
2025-02-13 10:29:25 +0200

    get sum annoying sheet

--- Script.js
+++ Script.js
@@ -1,6 +1,11 @@
 // this is the script
 console.log("hollopollo");
 
+const HowJammin = 800;
+var foodCount = 18;
+var AAA = ["Rodrigez", "Kabir", "Jamal", "William"] 
+var BBB = ["Tacos", "Kebab curry", "KFC", "Beans on toast"]
+
 /////}
 
 function toggleMute() {
@@ -27,4 +32,26 @@ function showVideo() {
     element.style.display = 'block';
     button.style.display = 'none';
     delay(100).then(() => toggleMute());
+}
+
+function Jammin(a){
+    if(a == HowJammin){
+        alert("Correct");
+    }else if(a > HowJammin){
+        alert("not yet.");
+    }else if(a < HowJammin){
+        alert("Longer than that");
+    }else{
+        alert("Nuh uh")
+    }
+}
+
+function Teht(a){
+    alert(`you ate ${a} apples, leaving  ${(foodCount-a)}  apples left`)
+}
+
+function Teht2(){
+    for (i=0; i<AAA.length; i++){
+        if (i == 1){alert(`${AAA[i]} ate ${foodCount-i} ${BBB[i]}.`)}else if (i == 2){alert(`${AAA[i]} ate ${(foodCount-(i*5))} ${BBB[i]}.`)}else if (i == 3){alert(`${AAA[i]} ate ${(foodCount-(i**2))} ${BBB[i]}.`)}else{alert(`${AAA[i]} ate ${(foodCount-(i**i))} ${BBB[i]}.`)}
+    }
 }
