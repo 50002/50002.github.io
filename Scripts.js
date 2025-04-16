321780f216ca2d80d311d1ece19c4bdc5748949d
50002 <142967542+50002@users.noreply.github.com>
2025-04-16 09:49:30 +0300

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,6 +1,7 @@
 
 var testi = document.getElementById("123")
 
+
 let recipies = {
     keksit: {
         Batch_Size: 20,
@@ -46,5 +47,127 @@ function myFunction() {
       result += "<li>" + d[document.getElementById("cars").value];
 });
     
-    document.getElementById("demo").innerHTML = result;
+    document.getElementById("demo").innerHTML = result
+    Laskin()
   }
+
+  function Laskin() {
+
+    switch (document.getElementById("cars").value){
+        case "keksit":
+            document.getElementById("yes").style.display = "block"
+            document.getElementById("keksikuva").style.visibility = "visible";
+            document.getElementById("ritarikuva").style.visibility = "hidden";
+            document.getElementById("lettukuva").style.visibility = "hidden";
+            document.getElementById("aines1").style.display = "block"
+            document.getElementById("aines2").style.display = "block"
+            document.getElementById("aines3").style.display = "block"
+            document.getElementById("aines4").style.display = "block"
+            document.getElementById("aines5").style.display = "block"
+            document.getElementById("aines6").style.display = "block"
+            document.getElementById("aines7").style.display = "block"
+            document.getElementById("aines8").style.display = "block"
+            document.getElementById("aines9").style.display = "none"
+            document.getElementById("aines10").style.display = "none"
+            document.getElementById("aines11").style.display = "none"
+            document.getElementById("aines12").style.display = "none"
+ 
+            document.getElementById("aines01").style.display = "block"
+            document.getElementById("aines21").style.display = "block"
+            document.getElementById("aines31").style.display = "block"
+            document.getElementById("aines41").style.display = "block"
+            document.getElementById("aines51").style.display = "block"
+            document.getElementById("aines61").style.display = "block"
+            document.getElementById("aines71").style.display = "block"
+            document.getElementById("aines81").style.display = "block"
+            document.getElementById("aines91").style.display = "none"
+            document.getElementById("aines101").style.display = "none"
+            document.getElementById("aines111").style.display = "none"
+            document.getElementById("aines121").style.display = "none"
+            var a = [document.getElementById("aines1").value/125, document.getElementById("aines2").value/(2.5*20*4), document.getElementById("aines3").value/1, document.getElementById("aines4").value/(1*0.5), document.getElementById("aines5").value/(0.5*2.5), document.getElementById("aines6").value/(2.5*25), document.getElementById("aines7").value/(2*65), document.getElementById("aines8").value/(0.33*5)]
+            var b = Math.min.apply(null, a)
+            console.log(a)
+            console.log(b)
+            document.getElementById("maara").innerHTML = "Arvioitu valmistus määränne on " + String(Math.floor(b*20))
+            return console.log(Math.floor(b*20));
+ 
+        case "letut":
+            document.getElementById("yes").style.display = "block"
+            document.getElementById("keksikuva").style.visibility = "hidden";
+            document.getElementById("ritarikuva").style.visibility = "hidden";
+            document.getElementById("lettukuva").style.visibility = "visible";
+            document.getElementById("aines1").style.display = "none"
+            document.getElementById("aines2").style.display = "block"
+            document.getElementById("aines3").style.display = "block"
+            document.getElementById("aines4").style.display = "none"
+            document.getElementById("aines5").style.display = "none"
+            document.getElementById("aines6").style.display = "none"
+            document.getElementById("aines7").style.display = "block"
+            document.getElementById("aines8").style.display = "none"
+            document.getElementById("aines9").style.display = "block"
+            document.getElementById("aines10").style.display = "block"
+            document.getElementById("aines11").style.display = "none"
+            document.getElementById("aines12").style.display = "none"
+ 
+            document.getElementById("aines01").style.display = "none"
+            document.getElementById("aines21").style.display = "block"
+            document.getElementById("aines31").style.display = "block"
+            document.getElementById("aines41").style.display = "none"
+            document.getElementById("aines51").style.display = "none"
+            document.getElementById("aines61").style.display = "none"
+            document.getElementById("aines71").style.display = "block"
+            document.getElementById("aines81").style.display = "none"
+            document.getElementById("aines91").style.display = "block"
+            document.getElementById("aines101").style.display = "block"
+            document.getElementById("aines111").style.display = "none"
+            document.getElementById("aines121").style.display = "none"
+            var c = [document.getElementById("aines2").value/(1*3*4), document.getElementById("aines3").value/3, document.getElementById("aines7").value/(3*65), document.getElementById("aines9").value/(0.6), document.getElementById("aines10").value/5]
+            var d =Math.min.apply(null, c)
+            console.log(c)
+            console.log(d)
+            document.getElementById("maara").innerHTML = "Arvioitu valmistus määränne on " + String(Math.floor(d*60))
+            return console.log(Math.floor(d*60))
+ 
+        case "ritarit":
+            document.getElementById("yes").style.display = "block"
+            document.getElementById("keksikuva").style.visibility = "hidden";
+            document.getElementById("ritarikuva").style.visibility = "visible";
+            document.getElementById("lettukuva").style.visibility = "hidden";
+            document.getElementById("aines1").style.display = "none"
+            document.getElementById("aines2").style.display = "none"
+            document.getElementById("aines3").style.display = "block"
+            document.getElementById("aines4").style.display = "block"
+            document.getElementById("aines5").style.display = "none"
+            document.getElementById("aines6").style.display = "none"
+            document.getElementById("aines7").style.display = "none"
+            document.getElementById("aines8").style.display = "none"
+            document.getElementById("aines9").style.display = "none"
+            document.getElementById("aines10").style.display = "none"
+            document.getElementById("aines11").style.display = "block"
+            document.getElementById("aines12").style.display = "block"
+ 
+            document.getElementById("aines01").style.display = "none"
+            document.getElementById("aines21").style.display = "none"
+            document.getElementById("aines31").style.display = "block"
+            document.getElementById("aines41").style.display = "block"
+            document.getElementById("aines51").style.display = "none"
+            document.getElementById("aines61").style.display = "none"
+            document.getElementById("aines71").style.display = "none"
+            document.getElementById("aines81").style.display = "none"
+            document.getElementById("aines91").style.display = "none"
+            document.getElementById("aines101").style.display = "none"
+            document.getElementById("aines111").style.display = "block"
+            document.getElementById("aines121").style.display = "block"
+            var e = [document.getElementById("aines3").value/1, document.getElementById("aines4").value/0.5, document.getElementById("aines11").value/2, document.getElementById("aines12").value/360]
+            var f = Math.min.apply(null, e)
+            console.log(e)
+            console.log(f)
+            document.getElementById("maara").innerHTML = "Arvioitu valmistus määränne on " + String(Math.floor(f*10))
+            return console.log(Math.floor(f*10))
+    }
+  }
+
+
+
+
+
